39f8d74c258b4cf087e1bb4a4a639bb128edc32a
mooyg <mooybot@gmail.com>
2024-05-09 23:42:22 +0530

    fix: forgot to pass special args to home manager

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -16,8 +16,9 @@ in
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+          # home-manager.useUserPackages = true;
 
+          home-manager.extraSpecialArgs = {inherit inputs myvars;};
           home-manager.users."${myvars.username}".imports = home-modules;
         }
       ];
