d3f2024ae1921265df411e8f3276f2785f08688d
mooyg <mooybot@gmail.com>
2024-05-09 23:45:07 +0530

    fix: forgot to pass special args to home manager

--- modules/nixos/base/users.nix
+++ modules/nixos/base/users.nix
@@ -1,11 +1,12 @@
 {lib, ...}: let
   myvars = import ../../../vars {inherit lib;};
 in {
+  users.mutableUsers = false;
   users.users."${myvars.username}" = {
     home = "/home/${myvars.username}";
     name = "${myvars.username}";
-    isSystemUser = true;
-    extraGroups = ["networkmanager" "wheel"];
+    isNormalUser = true;
+    extraGroups = ["networkmanager" "wheel" "users"];
   };
   users.users.${myvars.username}.group = "mooy";
   users.groups.${myvars.username} = {};
