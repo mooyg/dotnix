998df578e1ddaa0e735fdf74c21826ee64112d6d
mooyg <mooybot@gmail.com>
2024-05-10 22:08:48 +0530

    feat: add timezone

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,6 +1,6 @@
 {...}: {
   system.stateVersion = "24.05";
 
-  services.automatic-timezoned.enable = true;
+  time.timeZone = "Asia/Kolkata";
   imports = [./base/networking.nix ./base/nix.nix ./base/ssh.nix ./base/tailscale.nix ./base/packages.nix ./base/users.nix ./base/docker.nix];
 }
