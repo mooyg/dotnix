0625d8a0c11feb5b5935e5bf999d72b98476d788
mooyg <mooybot@gmail.com>
2024-05-10 16:37:58 +0530

    fix: no hardware mounting for now

--- hosts/idklaptop/default.nix
+++ hosts/idklaptop/default.nix
@@ -2,6 +2,10 @@
 {...}: let
   hostname = "idklaptop";
 in {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
