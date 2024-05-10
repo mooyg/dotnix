ec7e4dab2ac720d42769e1de307ebcb56e7bcf05
mooyg <mooybot@gmail.com>
2024-05-10 22:04:19 +0530

    test: enable firewall to check through k3s

--- modules/nixos/base/networking.nix
+++ modules/nixos/base/networking.nix
@@ -1,5 +1,5 @@
 {...}: {
-  networking.firewall.enable = false;
+  networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [22];
   networking.firewall.allowPing = true;
   # Samba discovery of machines and shares may need the firewall to be tuned
