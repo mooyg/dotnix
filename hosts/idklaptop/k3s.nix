e0d6603939ba6d090e233b2e57b4352e14ecd3b2
mooyg <mooybot@gmail.com>
2024-05-10 21:31:35 +0530

    bump

--- hosts/idklaptop/k3s.nix
+++ hosts/idklaptop/k3s.nix
@@ -7,6 +7,6 @@
     role = "agent";
     serverAddr = "https://100.71.18.118:6443";
     # Idk this should matter here since everything is protected but please don't mind.
-    tokenFile = "/etc/nixos/k3stoken";
+    token = "K10a6f7e08840e302cff9586ba60f50da39681388bc39ddc00efd7dd9f8dd41f047::server:db68f3c23c565d8ed6df645b642f8f53";
   };
 }
