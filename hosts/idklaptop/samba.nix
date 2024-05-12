8a00adddbd871500507c2f82928388021c0a89ae
mooyg <mooybot@gmail.com>
2024-05-12 10:32:16 +0530

    feat: new mount locations

--- hosts/idklaptop/samba.nix
+++ hosts/idklaptop/samba.nix
@@ -1,6 +1,13 @@
 {...}: {
-  fileSystems."/mnt/tvshows" = {
+  fileSystems."/mnt/homepi/tvshows" = {
     device = "//100.71.18.118/tvshows";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000"];
+  };
+  fileSystems."/mnt/homepi/movies" = {
+    device = "//100.71.18.118/movies";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
