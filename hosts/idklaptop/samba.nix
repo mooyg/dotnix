78a8a3eeafddb04af2a675c21cd8daa782cbbf0f
mooyg <mooybot@gmail.com>
2024-05-12 12:35:05 +0530

    fix: change mount drive

--- hosts/idklaptop/samba.nix
+++ hosts/idklaptop/samba.nix
@@ -1,13 +1,6 @@
 {...}: {
-  fileSystems."/mnt/homepi/tvshows" = {
-    device = "//100.71.18.118/tvshows";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000"];
-  };
-  fileSystems."/mnt/homepi/movies" = {
-    device = "//100.71.18.118/movies";
+  fileSystems."/mnt/homepi/" = {
+    device = "//100.71.18.118/homepi";
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
