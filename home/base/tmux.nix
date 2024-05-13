8ab30dc2d0ebdeb088f6fa6f80d7d483b5d5eade
mooyg <mooybot@gmail.com>
2024-05-13 19:14:06 +0530

    chore: bump theme settings

--- home/base/tmux.nix
+++ home/base/tmux.nix
@@ -2,7 +2,20 @@
   programs.tmux = {
     enable = true;
     keyMode = "vi";
-    plugins = with pkgs.tmuxPlugins; [dracula better-mouse-mode];
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = dracula;
+        extraConfig = ''
+                         set -g @dracula-show-battery false
+                         set -g @dracula-show-powerline true
+                         set -g @dracula-refresh-rate 10
+                      set -g @dracula-show-location false
+                    set -g @dracula-show-fahrenheit false
+          set -g @dracula-show-left-icon session
+        '';
+      }
+      better-mouse-mode
+    ];
     prefix = "C-w";
     extraConfig = ''
       set-option -ga terminal-overrides "alacritty:Tc"
