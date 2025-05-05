f83e1760684be4482f2c8bf80de7c1afcc13c373
mooyg <mooybot@gmail.com>
2025-05-05 21:45:14 +0530

    feat: add sops nix

--- modules/nixos/base/users.nix
+++ modules/nixos/base/users.nix
@@ -9,7 +9,7 @@ in {
   users.users."${myvars.username}" = {
     home = "/home/${myvars.username}";
     name = "${myvars.username}";
-    hashedPassword = "$6$orQz5xGVY4R9AxWe$ZB.JwHeqPPU3F.cZ.x5n3yZereBItTK3.3Dg.4nhIxF4C.SJ4gJQY4IZhLAtOdLXRbMliIE5B2eY2YQTs1F.E0";
+    passwordFile = config.sops.secrets.user_password.path;
     isNormalUser = true;
     extraGroups = ["networkmanager" "wheel" "users"];
     group = "mooy";
