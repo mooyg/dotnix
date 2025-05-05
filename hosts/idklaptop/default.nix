02606c3095a0f4b656e7090e8ac47641a90b019e
mooyg <mooybot@gmail.com>
2025-05-05 19:16:34 +0530

    bump

--- hosts/idklaptop/default.nix
+++ hosts/idklaptop/default.nix
@@ -2,7 +2,7 @@
 {...}: let
   hostname = "idklaptop";
 in {
-  imports = [./nvidia.nix ./docker.nix ,./hardware.nix];
+  imports = [./nvidia.nix ./docker.nix ./hardware.nix];
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
