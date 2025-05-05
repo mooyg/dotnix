983a6dd38a0364213e9305ad70a677708bbabacd
mooyg <mooybot@gmail.com>
2025-05-05 23:22:16 +0530

    refactor: sops logic

--- home/nixos/core/core.nix
+++ home/nixos/core/core.nix
@@ -1,7 +1,8 @@
-{pkgs, ...}: {
+{pkgs, inputs, ...}: {
   home.packages = with pkgs; [
     iputils
     btop 
     discord
   ];
+  imports = [ inputs.sops-nix.nixosModules.sops ];
 }
--- hosts/idklaptop/sops.nix
+++ hosts/idklaptop/sops.nix
@@ -1,9 +1,6 @@
-{ inputs, ... }:
+{  ... }:
 {
-  imports = [ inputs.sops-nix.nixosModules.sops ];
-
   sops.secrets.user_password = {
     sopsFile = ../../../secrets/secrets.yaml;
   };
-
 }
