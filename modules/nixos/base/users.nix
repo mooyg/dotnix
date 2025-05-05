d4f417f7a739a5702fdfd68d7cbf17e88e584331
mooyg <mooybot@gmail.com>
2025-05-05 20:19:47 +0530

    hardware config bump

--- modules/nixos/base/users.nix
+++ modules/nixos/base/users.nix
@@ -9,7 +9,7 @@ in {
   users.users."${myvars.username}" = {
     home = "/home/${myvars.username}";
     name = "${myvars.username}";
-    hashedPassword = "$7$CU..../....EyeSvcoNhIJLQLcf329k7/$K0p5Oda/2bNLksyoZSpxp3pyvXrH4SKdt3LPhlNnBQ4";
+    hashedPassword = "$6$orQz5xGVY4R9AxWe$ZB.JwHeqPPU3F.cZ.x5n3yZereBItTK3.3Dg.4nhIxF4C.SJ4gJQY4IZhLAtOdLXRbMliIE5B2eY2YQTs1F.E0";
     isNormalUser = true;
     extraGroups = ["networkmanager" "wheel" "users"];
     group = "mooy";
