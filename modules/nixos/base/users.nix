c856246aca362497514f5abe185a5fa2d0973916
mooyg <mooybot@gmail.com>
2025-05-05 21:58:44 +0530

    update

--- modules/nixos/base/users.nix
+++ modules/nixos/base/users.nix
@@ -6,6 +6,11 @@
 }: let
   myvars = import ../../../vars {inherit lib;};
 in {
+  sops.secrets.user_password = {
+    sopsFile = ../../../secrets/secrets.yaml;
+    neededForUsers = true;
+  };
+
   users.mutableUsers = false;
   users.users."${myvars.username}" = {
     home = "/home/${myvars.username}";
