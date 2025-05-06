d83cec8c8b554886bedd9bbc4ca5f375ec2d2f20
mooyg <mooybot@gmail.com>
2025-05-06 21:26:11 +0530

    chore: pass special args to nixos

--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -5,21 +5,21 @@
   home-modules,
   myvars,
   ...
-}: let
+}:
+let
   inherit (inputs) nixpkgs home-manager;
 in
-  nixpkgs.lib.nixosSystem {
-    inherit system;
-    modules =
-      nixos-modules
-      ++ [
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          # home-manager.useUserPackages = true;
+nixpkgs.lib.nixosSystem {
+  inherit system;
+  specialArgs = { inherit inputs myvars; };
+  modules = nixos-modules ++ [
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      # home-manager.useUserPackages = true;
 
-          home-manager.extraSpecialArgs = {inherit inputs myvars;};
-          home-manager.users."${myvars.username}".imports = home-modules;
-        }
-      ];
-  }
+      home-manager.extraSpecialArgs = { inherit inputs myvars; };
+      home-manager.users."${myvars.username}".imports = home-modules;
+    }
+  ];
+}
