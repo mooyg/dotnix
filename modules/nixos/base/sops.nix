be8b6c9d65359bda6abaef247e1320c96b1ba2b8
mooyg <mooybot@gmail.com>
2025-05-06 21:37:40 +0530

    bump

--- modules/nixos/base/sops.nix
+++ modules/nixos/base/sops.nix
@@ -1,9 +1,11 @@
 { specialArgs, ... }:
 {
   imports = [ specialArgs.inputs.sops-nix.nixosModules.sops ];
-  sops.secrets.user_password = {
-    sopsFile = ../../../secrets.yaml;
+  sops = {
     # TODO: dont hardcode this
-    age.keyFile = "/home/mooy/.config/sops/age/keys.txt" ;
+    age.keyFile = "/home/mooy/.config/sops/age/keys.txt";
+    secrets.user_password = {
+      sopsFile = ../../../secrets.yaml;
+    };
   };
 }
