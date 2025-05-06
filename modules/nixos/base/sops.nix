ab6b254c5124a17d8b6e09c5259ac844f93eb3c2
mooyg <mooybot@gmail.com>
2025-05-06 21:34:55 +0530

    fix: add keyfile path

--- modules/nixos/base/sops.nix
+++ modules/nixos/base/sops.nix
@@ -3,5 +3,7 @@
   imports = [ specialArgs.inputs.sops-nix.nixosModules.sops ];
   sops.secrets.user_password = {
     sopsFile = ../../../secrets.yaml;
+    # TODO: dont hardcode this
+    age.keyFile = "/home/mooy/.config/sops/age/keys.txt" ;
   };
 }
