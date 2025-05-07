57d43690c87678c7e0e7ac8d2676433b3bf2f11f
mooyg <mooybot@gmail.com>
2025-05-07 14:43:45 +0530

    chore: update nix version

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     darwin = {
       url = "github:lnl7/nix-darwin";
