90e155f52440df730a8766835129f5e4dddf9411
mooyg <mooybot@gmail.com>
2025-05-07 13:58:12 +0530

    bump

--- home/base/neovim/plugins/lsp.nix
+++ home/base/neovim/plugins/lsp.nix
@@ -16,14 +16,10 @@
       enable = true;
     };
     tailwindcss.enable = true;
-    prismals.enable = true;
-    svelte.enable = true;
     jsonls.enable = true;
     nil_ls.enable = true;
     rust-analyzer.enable = true;
     taplo.enable = true;
-    volar.enable = true;
-    marksman.enable = true;
     gopls = {
       enable = true;
       package = null;
